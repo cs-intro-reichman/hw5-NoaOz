4990c02b3923d231f95c9eff3343be2f0521470f
NoaOz <noa.oz01@post.runi.ac.il>
2024-12-12 22:57:56 +0200

    com SSS

--- In.java
+++ In.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.Socket;
+import java.net.URI;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -177,6 +178,7 @@ public final class In {
 
             // or URL from web
             if (url == null) {
+                url = new URL(name);
             }
 
             URLConnection site = url.openConnection();
